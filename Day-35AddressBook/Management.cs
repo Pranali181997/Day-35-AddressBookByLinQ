9bc8df57da4821afc0d4714b1fa1708faa5a0f37
Pranali <pranalilambat00@gmail.com>
2022-03-30 17:51:13 +0530

    CreateDatatBase

---
+++ Day-35AddressBook/Management.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Day_35AddressBook
+{
+    public class Management
+    {
+        //UC-1-Create datatable 
+        public static DataTable BookTable = new DataTable();
+        public static void AddTable()
+        {
+
+            //UC-2-Insert The columns
+            BookTable.Columns.Add("FirstName", typeof(string));
+            BookTable.Columns.Add("LastName", typeof(string));
+            BookTable.Columns.Add("Address", typeof(string));
+            BookTable.Columns.Add("City", typeof(string));
+            BookTable.Columns.Add("State", typeof(string));
+            BookTable.Columns.Add("Zip", typeof(string));
+            BookTable.Columns.Add("PhoneNumber", typeof(string));
+            BookTable.Columns.Add(" Email", typeof(string));
+
+            Console.WriteLine("\nDataTable contents:");
+            foreach (var v in BookTable.AsEnumerable())
+            {
+                Console.WriteLine($"ProductID:{v.Field<string>("FirstName")}\tUserID:{v.Field<string>("LastName")}\tRating:{v.Field<string>("Address")}\tReview:{v.Field<string>("City")}\tIsLike:{v.Field<string>("State")}");
+
+            }
+
+        }
+    }
+}
